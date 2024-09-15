ca83e74b8cba471365caf21c3b9b20e2d1873ba0
michaelyliu6 <52585777+michaelyliu6@users.noreply.github.com>
2024-09-15 01:31:00 -0400

    adding gradient accumulation

--- train_gpt2.py
+++ train_gpt2.py
@@ -57,7 +57,7 @@ class CausalSelfAttention(nn.Module):
          # memory efficient way of calculating attention without storing full attention matrix 
          # recomputes certain values as needed
          # better utilizes accesses to GPU memory bandwidth 
-        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        y = nn.functional.scaled_dot_product_attention(q, k, v, is_causal=True)
         
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
@@ -223,8 +223,8 @@ model_args = {
     'n_embd': 768,
 } # for the 124M parameter GPT model 
 
-scratch_model = GPT(model_args)
-sd = scratch_model.state_dict()
+model = GPT(model_args)
+sd = model.state_dict()
 
 
 sd_keys = sd.keys()
@@ -336,8 +336,15 @@ print(f"using device: {device}")
 
 
 
-train_loader = DataLoaderLite(B=16, T=1024) # generated x and y of shape [16, 1024] 
+total_batch_size = 524288 # 2**19, ~0.5M, in number of tokens
+B = 16 # micro batch size
+T = 1024 # sequence length
+assert total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T"
+grad_accum_steps = total_batch_size // (B * T)
+print(f"total desired batch size: {total_batch_size}")
+print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
 
+train_loader = DataLoaderLite(B=B, T=T)
 
 
 # used for unlocking better performance by better utilizing the GPU
@@ -345,12 +352,12 @@ train_loader = DataLoaderLite(B=16, T=1024) # generated x and y of shape [16, 10
 torch.set_float32_matmul_precision('high')
 
 
-scratch_model.eval()
-scratch_model.to(device)
+model.eval()
+model.to(device)
 
 # allow pytorch to automatically read through the model and make optimizations 
 # https://pytorch.org/docs/stable/generated/torch.compile.html
-scratch_model = torch.compile(model)
+model = torch.compile(model)
 
 
 
@@ -372,30 +379,44 @@ def get_lr(it):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
 
-optimizer = scratch_model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device)
 
 # Start a training loop that will run for 50 iterations
 for step in range(max_steps):
-    # fetch next batch of training data from DataLoaderLite
-    x, y = train_loader.next_batch()
-
-    # push training data to device
-    x, y = x.to(device), y.to(device)
-
 
     # Zero out the gradients from the previous iteration
     optimizer.zero_grad()
     
-    # Forward pass: compute the model's output (logits) and loss
-    # let pytorch selectively downcasting the precision of some operations to speed up training while maintaining accuracy and reducing memory usage 
-    with torch.autocast(device_type=device, dtype=torch.bfloat16): 
-        logits, loss = model(x, y)
+    loss_accum = 0.0
+
+    for micro_step in range(grad_accum_steps):
+        t0 = time.time()
+
+        # fetch next batch of training data from DataLoaderLite
+        x, y = train_loader.next_batch()
+
+        # push training data to device
+        x, y = x.to(device), y.to(device)
+
+        # Forward pass: compute the model's output (logits) and loss
+         # let pytorch selectively downcasting the precision of some operations to speed up training while maintaining accuracy and reducing memory usage 
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+
+        # we have to scale the loss to account for gradient accumulation,
+        # because the gradients just add on each successive backward().
+        # addition of gradients corresponds to a SUM in the objective, but
+        # instead of a SUM we want MEAN. Scale the loss here so it comes out right
+        loss = loss / grad_accum_steps
     
-    # Backward pass: compute gradients of the loss with respect to model parameters
-    loss.backward()
+        # only keep track of the scalar loss, but the gradient graph
+        loss_accum += loss.detach()
+
+        loss.backward()
 
     # Gradient clipping: If the norm exceeds the specified maximum (1.0 in this case), it scales down all gradients proportionally so that their norm equals the maximum.
     # Stablize training by prevent gradients from becoming too large (exploding gradients) 
+    # norm is the total of the norms of all the gradients before clipping
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
     # determine and set the learning rate for this iteration
@@ -412,9 +433,9 @@ for step in range(max_steps):
      # calculate time difference in miliseconds
     t1 = time.time()
     dt = (t1 - t0)*1000 # time difference in miliseconds
-    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {step:4d} | loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
-
+    tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
+    tokens_per_sec = tokens_processed / dt
+    print(f"step {step:4d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 # import tiktoken
 
@@ -436,7 +457,7 @@ for step in range(max_steps):
 
 # with torch.no_grad():
 #     while x.size(1) < max_sequence_length: # keep generating next token until max_sequence_length is reached
-#         logits = scratch_model(x)
+#         logits = model(x)
 #         # [batch_size, block_size, vocab_size] -> [batch_size, block_size, vocab_size] 
 
 #         logits = logits[:,-1,:]
